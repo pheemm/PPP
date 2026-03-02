5fab72f0829c10244ed012e50da33b33b1462234
pheemm <eaa29101975@gmail.com>
2026-03-02 17:07:06 +0300

    Adding simple work with weather API
    Creatng simple code and importing special library for weather API. Without any connecting with other parts

--- main.py
+++ main.py
@@ -1,35 +1,21 @@
-#!/usr/bin/env python
-# pylint: disable=unused-argument
-# This program is dedicated to the public domain under the CC0 license.
-
-"""
-Basic example for a bot that uses inline keyboards. For an in-depth explanation, check out
- https://github.com/python-telegram-bot/python-telegram-bot/wiki/InlineKeyboard-Example.
-"""
-
 import logging
+
+import requests
 
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import Application, CallbackQueryHandler, CommandHandler, ContextTypes
 
-# Enable logging
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
-# set higher logging level for httpx to avoid all GET and POST requests being logged
 logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Sends a message with three inline buttons attached."""
     keyboard = [
-        [
-            InlineKeyboardButton("Option 1", callback_data="1"),
-            InlineKeyboardButton("Option 2", callback_data="2"),
-        ],
-        [InlineKeyboardButton("Option 3", callback_data="3")],
+            InlineKeyboardButton("Узнать погоду", callback_data="1"),
     ]
 
     reply_markup = InlineKeyboardMarkup(keyboard)
@@ -38,31 +24,31 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 
 async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Parses the CallbackQuery and updates the message text."""
     query = update.callback_query
 
-    # CallbackQueries need to be answered, even if no notification to the user is needed
-    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
     await query.answer()
 
     await query.edit_message_text(text=f"Selected option: {query.data}")
 
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Displays info on how to use the bot."""
     await update.message.reply_text("Use /start to test this bot.")
 
 
 def main() -> None:
-    """Run the bot."""
-    # Create the Application and pass it your bot's token.
+    param = {'key': 'zzbbotd6xlg0go56sl3qyn4vocb4b8z0q1h3mqq5',
+             'place_id': 'moscow'}
+    url = 'https://www.meteosource.com/api/v1/free/point'
+
+    data = requests.get(url, param).json()
+
+
     application = Application.builder().token("8716402003:AAEMQKV0rSgCq5Yv6p8tCyRULu7tfpxokWI").build()
 
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CallbackQueryHandler(button))
     application.add_handler(CommandHandler("help", help_command))
 
-    # Run the bot until the user presses Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
